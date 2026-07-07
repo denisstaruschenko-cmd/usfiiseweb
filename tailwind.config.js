979f320d2ae5381859cd1cf02357b4df5eb120b3
denisstaruschenko-cmd <denis.staruschenko@gmail.com>
2026-07-07 16:36:42 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+export default {
+
+  content: [
+
+    "./index.html",
+
+    "./src/**/*.{js,jsx}"
+
+  ],
+
+  theme: {
+
+    extend: {
+
+      colors: {
+
+        primary:"#1B3A6B",
+
+        secondary:"#2563EB",
+
+        accent:"#F3F4F6"
+
+      }
+
+    }
+
+  },
+
+  plugins:[]
+}
